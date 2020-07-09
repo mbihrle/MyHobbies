b59472235011648993dab91de74f6d91937bdbce
mbihrle <bihrle.marco@hotmail.com>
2020-07-09 17:21:25 +0200

    installation of intervention image

--- app/Http/Controllers/HobbyController.php
+++ app/Http/Controllers/HobbyController.php
@@ -7,6 +7,7 @@ use App\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Session;
+use Intervention\Image\Facades\Image;
 
 class HobbyController extends Controller
 {
